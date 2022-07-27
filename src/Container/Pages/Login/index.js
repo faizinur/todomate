8ceea844df7d67bcf3c6f9787e6e9ace56074974
ifaizinur <inurfaizi@gmail.comm>
2022-07-27 12:38:12 +0700

    redesign Splash

--- src/Container/Pages/Login/index.js
+++ src/Container/Pages/Login/index.js
@@ -1,4 +1,4 @@
-import { View, Dimensions, ImageBackground, StatusBar, Text } from 'react-native'
+import { View, Dimensions, ImageBackground, StatusBar } from 'react-native'
 import React, { useEffect } from 'react'
 import { log } from '@Utils'
 import { useTheme, TextInput, Button } from 'react-native-paper';
@@ -18,15 +18,13 @@ export default ({ navigation: { replace } }) => {
         <PageWrapper>
             <StatusBar backgroundColor={`${colors.shark}de`} barStyle={'light-content'} />
             <ImageBackground
-                // source={{ uri: `https://images.unsplash.com/photo-1489641024260-20e5cb3ee4aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1633&q=80` }}
-                // source={{ uri: `https://images.unsplash.com/photo-1468434453985-b1ca3b555f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&w=1170&q=1250` }}
                 source={{ uri: `https://images.unsplash.com/photo-1486162928267-e6274cb3106f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&w=688&q=1250` }}
                 style={{ height: height - StatusBar.currentHeight, width, backgroundColor: colors.zircon }}
                 resizeMethod={'scale'}
                 imageStyle={{ opacity: .9, backgroundColor: colors.shark }}
                 resizeMode={'cover'}>
                 <View style={{ flex: 1, justifyContent: 'space-between', alignItems: 'flex-start', paddingHorizontal: '5%', paddingVertical: '10%' }}>
-                    <MyText fontSize={45} bold color={colors.zircon} opacity={.8} numberOfLines={6}>Bring wacana to reality{`\n\n`}<MyText large color={colors.shark} opacity={.8}>    </MyText></MyText>
+                    <MyText fontSize={45} bold color={colors.zircon} opacity={.9} numberOfLines={6}>Bring wacana to reality{`\n\n`}<MyText large color={colors.shark} opacity={.8}>    </MyText></MyText>
                     <View style={{ borderRadius: 15, overflow: "hidden", flex: 1, width: "100%" }}>
                         <BlurView
                             blurType={"dark"}
@@ -35,9 +33,10 @@ export default ({ navigation: { replace } }) => {
                             style={{ flex: 1 }}>
                             <View style={{ flex: 1, backgroundColor: 'transparent', padding: '5%', justifyContent: 'center' }}>
                                 <MyText fontSize={35} color={colors.zircon} opacity={.9} style={{ marginVertical: 5 }}>Hi!</MyText>
-                                <MyText small color={colors.zircon} opacity={.9} style={{ marginBottom: 20 }}>Gak punya akun? <MyText small color={colors.caribbeanGreen} onPress={() => log('Daftar')} >sini daftar dulu.</MyText></MyText>
+                                <MyText small color={colors.zircon} opacity={.9} style={{ marginBottom: 20 }}>Gak punya akun? <MyText small bold color={colors.caribbeanGreen} onPress={() => log('Daftar')} >sini daftar dulu.</MyText></MyText>
                                 <View style={{ width: '100%', marginVertical: 5 }}>
                                     <TextInput
+                                        placeholderTextColor={`${colors.shark}cd`}
                                         theme={{
                                             colors: {
                                                 primary: colors.caribbeanGreen
@@ -50,11 +49,12 @@ export default ({ navigation: { replace } }) => {
                                             }
                                         }}
                                         mode={'outlined'} placeholder={'Nama Pengguna'}
-                                        style={{ backgroundColor: colors.zircon, fontSize: 12 }}
+                                        style={{ backgroundColor: colors.zircon, opacity: .9, fontSize: 12 }}
                                     />
                                 </View>
                                 <View style={{ width: '100%', marginVertical: 5 }}>
                                     <TextInput
+                                        placeholderTextColor={`${colors.shark}cd`}
                                         theme={{
                                             colors: {
                                                 primary: colors.caribbeanGreen
@@ -67,7 +67,7 @@ export default ({ navigation: { replace } }) => {
                                             }
                                         }}
                                         mode={'outlined'} placeholder={'Kata Sandi'}
-                                        style={{ backgroundColor: colors.zircon, fontSize: 12 }}
+                                        style={{ backgroundColor: colors.zircon, opacity: .9, fontSize: 12 }}
                                         secureTextEntry
                                     />
                                 </View>
@@ -75,13 +75,13 @@ export default ({ navigation: { replace } }) => {
                                     <Button
                                         mode="contained"
                                         labelStyle={{ color: colors.zircon, fontWeight: 'bold', fontFamily: 'ReadexProRegular', fontSize: 14 }}
-                                        contentStyle={{ height: 50 }}
+                                        contentStyle={{ height: 50, opacity: .9 }}
                                         onPress={() => replace('Main')}
                                         theme={{ colors: { primary: colors.caribbeanGreen } }}>
                                         Punten
                                     </Button>
                                 </View>
-                                <MyText small color={colors.zircon} opacity={.9} style={{ marginVertical: 15 }}>Gak bisa masuk? <MyText onPress={() => log('Reset')} small color={colors.caribbeanGreen}>kesini dulu deh.</MyText></MyText>
+                                <MyText small color={colors.zircon} opacity={.9} style={{ marginVertical: 15 }}>Gak bisa masuk? <MyText onPress={() => log('Reset')} small bold color={colors.caribbeanGreen}>kesini dulu deh.</MyText></MyText>
                             </View>
                         </BlurView>
                     </View>
@@ -91,3 +91,6 @@ export default ({ navigation: { replace } }) => {
 
     )
 }
+
+// source={{ uri: `https://images.unsplash.com/photo-1489641024260-20e5cb3ee4aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1633&q=80` }}
+// source={{ uri: `https://images.unsplash.com/photo-1468434453985-b1ca3b555f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&w=1170&q=1250` }
--- src/Container/Pages/Splash/index.js
+++ src/Container/Pages/Splash/index.js
@@ -1,19 +1,28 @@
-import { View, Text, ActivityIndicator } from 'react-native'
+import { View, ActivityIndicator, Dimensions, ImageBackground, StatusBar } from 'react-native'
 import React, { useEffect } from 'react'
 import { useTheme } from 'react-native-paper';
-
+import { MyText } from '@Atoms'
 export default ({ navigation: { replace } }) => {
     const { colors } = useTheme();
+    const { height, width } = Dimensions.get('window')
     useEffect(() => {
-        setTimeout(() => replace('Main'), 1000)
+        setTimeout(() => replace('Login'), 1000)
         return () => {
 
         }
     }, [])
     return (
         <View style={{ flex: 1, backgroundColor: colors.zircon, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Splash</Text>
-            <ActivityIndicator style={{ position: 'absolute', bottom: 40 }} size={'large'} />
+            <StatusBar backgroundColor={`${colors.shark}de`} barStyle={'light-content'} />
+            <ImageBackground
+                source={{ uri: `https://images.unsplash.com/photo-1486162928267-e6274cb3106f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&w=688&q=1250` }}
+                style={{ height: height - StatusBar.currentHeight, width, backgroundColor: colors.zircon, justifyContent: 'center', alignItems: 'center' }}
+                resizeMethod={'scale'}
+                imageStyle={{ opacity: 1, backgroundColor: colors.shark }}
+                resizeMode={'cover'}>
+                <MyText fontSize={30} bold color={colors.zircon} opacity={.7} numberOfLines={6}>SPLASHSCREEN</MyText>
+                <ActivityIndicator style={{ position: 'absolute', bottom: 40 }} color={colors.caribbeanGreen} size={'large'} />
+            </ImageBackground>
         </View>
     )
 }
